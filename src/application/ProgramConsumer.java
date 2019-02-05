4d455d11e41bb61677d2a6b37f6734d4a62f6f30
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-02-05 11:30:33 -0200

    Consumer expressao lambda declarada

--- src/application/ProgramConsumer.java
+++ src/application/ProgramConsumer.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class ProgramConsumer {
 
@@ -19,7 +19,16 @@ public class ProgramConsumer {
 		products.add(new Product("Tablet", 350.50));
 		products.add(new Product("Hd Case", 89.90));
 		
-		products.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.10;
+		
+		//Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
+		
+		//usa chaves quando a temos mais de uma linha (mais comum)
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+		
+		products.forEach(cons);
 		
 		products.forEach (System.out::println);
 	}

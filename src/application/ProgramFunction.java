c49110c6dbf04b89ccd6c18e7074d90da4cc8a42
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-02-05 11:51:56 -0200

    Function implementacao da Interface

---
+++ src/application/ProgramFunction.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import util.UpperCaseName;
+
+public class ProgramFunction {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> products = new ArrayList<>();
+		
+		products.add(new Product("TV", 900.00));
+		products.add(new Product("Mouse", 50.00));
+		products.add(new Product("Tablet", 350.50));
+		products.add(new Product("Hd Case", 89.90));
+
+		//products.stream().map(new UpperCaseName()); // transformando em stream
+		                       // lista => stream                        |    stream  => lista
+		List<String> prodNames = products.stream().map(new UpperCaseName()).collect(Collectors.toList()); // lista para stream e stream para lista
+		
+		prodNames.forEach(System.out::println);
+	}
+
+}
